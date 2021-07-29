be2def98dd40d1a694f8a3c32ce71b3cb93d599a
Saarim Khan <saarimkhan515@gmail.com>
2021-07-30 01:11:37 +0530

    Intial Commit

---
+++ Assignments/Searching And Sorting/PushZerToEnd.cpp
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <math.h>
+#include <climits>
+using namespace std;
+
+void pushZeroesEnd(int *arr, int size)
+{
+    int i = 0;
+    int j = 0;
+
+    while (i < size)
+    {
+
+        if (arr[i] != 0)
+        {
+
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+
+            i++;
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+int main()
+{
+
+    ifstream in;
+    in.open("Input.txt");
+    ofstream out;
+    out.open("Output.txt");
+
+    int t;
+    in >> t;
+
+    while (t--)
+    {
+
+        int n;
+        in >> n;
+
+        int *arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            in >> arr[i];
+        }
+
+        pushZeroesEnd(arr, n);
+
+        for (int i = 0; i < n; i++)
+        {
+            out << arr[i] << " ";
+        }
+
+        out << endl;
+    }
+}
