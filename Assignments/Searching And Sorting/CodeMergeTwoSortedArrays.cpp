558f4477ac20ae1728521646174950fa9b5c6cd8
Saarim Khan <saarimkhan515@gmail.com>
2021-07-30 12:23:28 +0530

    Intial Commit

--- Assignments/Searching And Sorting/CodeMergeTwoSortedArrays.cpp
+++ Assignments/Searching And Sorting/CodeMergeTwoSortedArrays.cpp
@@ -40,30 +40,36 @@ void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
 int main()
 {
 
-    ifstream in;
-    in.open("Input.txt");
-    ofstream out;
-    out.open("Output.txt");
+    // ifstream in;
+    // in.open("Input.txt");
+    // ofstream out;
+    // out.open("Output.txt");
+
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+
     int t;
-    in >> t;
+    cin >> t;
     while (t--)
     {
         int n1, n2;
-        in >> n1;
+        cin >> n1;
         int *arr1 = new int[n1];
 
         for (int i = 0; i < n1; i++)
         {
-            in >> arr1[i];
+            cin >> arr1[i];
         }
 
-        in >> n2;
+        cin >> n2;
 
         int *arr2 = new int[n2];
 
         for (int i = 0; i < n2; i++)
         {
-            in >> arr2[i];
+            cin >> arr2[i];
         }
 
         int *ans = new int[n1 + n2];
@@ -72,9 +78,9 @@ int main()
 
         for (int i = 0; i < n1 + n2; i++)
         {
-            out << ans[i] << " ";
+            cout << ans[i] << " ";
         }
-        out << endl;
+        cout << endl;
     }
     return 0;
 }
